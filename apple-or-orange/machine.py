eab85967318c2f294fa38e6a2f93ba5f2f8a2da1
Douglas Daniel <douglascgd@gmail.com>
2017-05-16 21:16:51 -0300

    python files

---
+++ apple-or-orange/machine.py
@@ -0,0 +1,10 @@
+# features 1 = smooth, 0 = bumpy
+# labels 0 = apple, 1 = orange
+from sklearn import tree
+features = [[140, 1], [130, 1], [150, 0], [170, 0]]
+labels = [0, 0, 1, 1]
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(features, labels)
+print(clf.predict([[150, 0]]))
+
+
